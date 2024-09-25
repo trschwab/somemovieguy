88b7232bab662c644413d5dcdf9749489c7270c2
trschwab <trschwab7@gmail.com>
2024-09-24 23:38:06 -0400

    all up to date for deployment

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -2,10 +2,15 @@ import time
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build", static_url_path='/')
 CORS(app)  # Enable CORS
 
-@app.route('/time/')
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/time/')
 def get_current_time():
     return {'time': time.time()}
 
