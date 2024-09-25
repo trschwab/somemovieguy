9ccd18a52626fba9d4a24ec02a0eb8340a12675f
trschwab <trschwab7@gmail.com>
2024-09-24 23:12:19 -0400

    conclusion of front and back end code"

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -1,10 +1,13 @@
 import time
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS
 
-
-@app.route('/time')
+@app.route('/time/')
 def get_current_time():
-	return {'time': time.time()}
+    return {'time': time.time()}
 
+if __name__ == '__main__':
+    app.run(debug=True)
